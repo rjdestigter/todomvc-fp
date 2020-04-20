2fa274a239fdc952e0c02d6567383da56581b260
John de Stigter <johndestigter@gmail.com>
2020-04-19 18:55:00 -0600

    Updates

---
+++ precache-manifest.48bcc0813faafe742f87db361074e902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82acb9c2c5487b1c2829c6df898090a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d62eedb80b2ae2beb2",
+    "url": "/static/css/2.76c7595d.chunk.css"
+  },
+  {
+    "revision": "ea6a9b6e60c6a8de90ea",
+    "url": "/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "57d62eedb80b2ae2beb2",
+    "url": "/static/js/2.3083dc59.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/2.3083dc59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6a9b6e60c6a8de90ea",
+    "url": "/static/js/main.e9020577.chunk.js"
+  },
+  {
+    "revision": "068848b18f47253ce923",
+    "url": "/static/js/runtime-main.975dfb49.js"
+  }
+]);
